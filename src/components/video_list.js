21facccaafd404f0484c5f1e6afa7220fc388eb2
Conner Novicki <connernovicki@gmail.com>
2017-04-19 11:28:23 -0400

    first commit

---
+++ src/components/video_list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+  const video_list = props.videos.map((video, ind) => {
+    return <VideoListItem
+            video={video}
+            key={video.etag}/>;
+  });
+  return (
+    <ul className="col-md-4 list-group">
+    {video_list}
+    </ul>
+  );
+};
+
+export default VideoList;
